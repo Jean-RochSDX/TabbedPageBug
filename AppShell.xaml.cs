ddb5c704f44cbec0173d6a4224549e66b9f8b61d
Jean-Roch Boursiquot <jean-roch.boursiquot@sdx-ag.de>
2023-10-18 10:44:40 +0200

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestTabbedPage
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NewPage1.xaml.cs
@@ -0,0 +1,9 @@
+namespace TestTabbedPage;
+
+public partial class NewPage1 : TabbedPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
